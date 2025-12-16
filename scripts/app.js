36d6f4961a571b7174b9acd253164d9480b00fb4
kel <yaboimello89@gmail.com>
2025-12-16 15:42:14 -0800

    finally got data to work

--- scripts/app.js
+++ scripts/app.js
@@ -3,24 +3,24 @@ const currentTemp = document.getElementById("current-temp");
 const weatherIcon = document.getElementById("weather-icon");
 const todayTitle = document.getElementById("today-title");
 
+function getData() {
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
 
-function getData(){
-  navigator.geolocation.getCurrentPosition((position) => {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-  
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8661d7c45684fcb7eb85cafb35ec6eaf`;
-  
-      const response = fetch(url);
-      const data =  response.json;
-  
-      const weatherData = data;
-      
-      console.log(weatherData.main.temp);
-      return weatherData;
-      console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=8661d7c45684fcb7eb85cafb35ec6eaf`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    console.log(data);
+    
+    
+    currentTemp.textContent = `${data.main.temp}`;
+    city.textContent = `${data.name}`;
+    return data;
   });
-
 }
 
 getData();
+
